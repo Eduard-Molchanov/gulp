a5e4ee48f5881a929db419e1a96ae81ee67ddd0c
Eduard Molchanov <eduard.molchanov@gmail.com>
2016-12-22 22:58:21 +0300

    добавлены улучшения кода

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var g = require("gulp"),
+    p = require("gulp-load-plugins")(),
+    b = require("browser-sync"),
+    u = {
+        i: "app/index.html",
+        html: "app/pages",
+        css: "app/css",
+        mcss: "app/css/main.min.css",
+        scss: "app/scss/main.scss"
+    };
+
+g.task("s", function() {
+    b({
+        server: {
+            baseDir: "app/"
+        }
+    })
+});
+
+
+
+g.task("w", ["s"], function() {
+    g.watch(u.i, b.reload);
+    g.watch(u.html, b.reload);
+    g.watch(u.scss, ['scss']);
+    g.watch(u.mcss, b.reload);
+});
+
+g.task("scss", function() {
+    g.src(u.scss)
+        .pipe(p.sass())
+        .pipe(p.dest(u.css))
+        .pipe(p.cssnano())
+        .pipe(p.rename("main.min.css"))
+        .pipe(g.dest(u.css));
+});
+
+g.task("default", ["w"]);
